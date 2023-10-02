f6e14a2da156b93ddf340f8aaf4e0030680fb224
mulimuoki001 <b.muoki@alustudent.com>
2023-10-02 14:49:43 +0200

    updated test user file

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -16,19 +16,19 @@ class test_User(test_basemodel):
     def test_first_name(self):
         """ """
         new = self.value()
-        self.assertEqual(str(new.first_name))
+        self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
         """ """
         new = self.value()
-        self.assertEqual(str(new.last_name))
+        self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
         """ """
         new = self.value()
-        self.assertEqual(str(new.email))
+        self.assertEqual(type(new.email), str)
 
     def test_password(self):
         """ """
         new = self.value()
-        self.assertEqual(str(new.password))
+        self.assertEqual(type(new.password), str)
