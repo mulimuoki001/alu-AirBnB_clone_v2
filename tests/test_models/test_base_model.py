20400c3a19f6511c37c738d8fd08a440753d01a1
mulimuoki001 <b.muoki@alustudent.com>
2023-10-02 20:05:54 +0200

    updated base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -56,6 +56,13 @@ class test_basemodel(unittest.TestCase):
             j = json.load(f)
             self.assertEqual(j[key], i.to_dict())
 
+    def test_str(self):
+        new = self.value()
+        expected_str = "[User] ({}) {}".format(
+            new.id, {k: v for k, v in new.__dict__.items() if k != "_sa_instance_state"}
+        )
+        self.assertEqual(str(new), expected_str)
+
     def test_todict(self):
         """ """
         i = self.value()
