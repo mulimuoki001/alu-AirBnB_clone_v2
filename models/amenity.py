d4e57b09c3eaf152901ea0c07751737311cdca18
mulimuoki001 <b.muoki@alustudent.com>
2023-10-04 01:23:11 +0200

    updated files

--- models/amenity.py
+++ models/amenity.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
-
+from models.place import place_amenity
 
 class Amenity(BaseModel, Base):
     """Represents an Amenity for a MySQL database.
@@ -19,5 +19,4 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
+    place_amenities = relationship("Place", secondary="place_amenity")
