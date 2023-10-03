1daf14be7aefb5e0f7ea6d055fc804566bf5230c
mulimuoki001 <b.muoki@alustudent.com>
2023-10-04 00:30:59 +0200

    updated db storage file

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -38,25 +38,24 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Query on the curret database session all objects of the given class.
-
-        If cls is None, queries all types of objects.
-
-        Return:
-            Dict of queried classes in the format <class name>.<obj id> = obj.
-        """
-        if cls is None:
-            objs = self.__session.query(State).all()
-            objs.extend(self.__session.query(City).all())
-            objs.extend(self.__session.query(User).all())
-            objs.extend(self.__session.query(Place).all())
-            objs.extend(self.__session.query(Review).all())
-            objs.extend(self.__session.query(Amenity).all())
-        else:
-            if type(cls) == str:
+        """ """
+        if cls:
+            if type(cls) is str:
                 cls = eval(cls)
-            objs = self.__session.query(cls)
-        return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
+            query = self.__session.query(cls)
+            for ele in query:
+                key = "{}"."{}".format(type(ele).__name__, ele.id)
+                dic[key] = ele
+        else:
+            lists = [State, City, User, Place, Review, Amenity]
+            for classe in lists:
+                query = self.__session.query(classe)
+                for ele in query:
+                    key = "{}"."{}".format(type(ele)).__name__, ele.id
+                    dic[key] = ele
+
+        return (dic)
+            
 
     def new(self, obj):
         """Add obj to the current database session."""
