5bb6ea6bcafbd81b4d5a76a72acc96d570bcfc6a
mulimuoki001 <b.muoki@alustudent.com>
2023-10-04 10:49:25 +0200

    updated files

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """ Module for testing file storage"""
 import unittest
-import models
 
+import models
 from models.base_model import BaseModel
-from models import storage
 import os
 
 
