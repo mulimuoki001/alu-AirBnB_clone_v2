096f666729eb03070fe320dbc56c6f2f27faa547
mulimuoki001 <b.muoki@alustudent.com>
2023-10-21 23:11:08 +0200

    updated files

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,10 +10,11 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states():
     """Comment"""
-    return render_template("7-states_list.html", states=storage.all("State").values())
+    return render_template('7-states_list.html',
+                           states=storage.all('State').values())
 
 
 @app.teardown_appcontext
@@ -22,5 +23,5 @@ def teardown(self):
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -10,23 +10,29 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route("/states", strict_slashes=False)
+@app.route('/states', strict_slashes=False)
 def state_list():
     """Comment"""
-    states = storage.all("State").values()
-    return render_template("9-states.html", states=states, condition="states_list")
+    states = storage.all('State').values()
+    return render_template(
+        "9-states.html",
+        states=states,
+        condition="states_list")
 
 
-@app.route("/states/<id>", strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
 def states_by_id(id):
     """Comment"""
-    all_states = storage.all("State")
+    all_states = storage.all('State')
     key = "State.{}".format(id)
     try:
         state = all_states[key]
-        return render_template("9-states.html", state=state, condition="state_id")
+        return render_template(
+            '9-states.html',
+            state=state,
+            condition="state_id")
     except:
-        return render_template("9-states.html", condition="not_found")
+        return render_template('9-states.html', condition="not_found")
 
 
 @app.teardown_appcontext
@@ -35,5 +41,5 @@ def teardown(self):
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
